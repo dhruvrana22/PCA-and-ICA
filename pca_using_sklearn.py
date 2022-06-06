4a60ea6163d83cae38aeed888aedfccee6f405d3
Dhruv Rana <dhruvrana2001@gmail.com>
2022-06-06 11:28:55 +0530

    Create pca_using_sklearn.py

---
+++ pca_using_sklearn.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import seaborn as sb
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+data = pd.read_csv("eegData.csv",header=None)
+data.keys()
+scaling=StandardScaler()
+scaling.fit(data)
+Scaled_data=scaling.transform(data)
+tstart = datetime.datetime.now()
+principal=PCA(n_components=2)
+principal.fit(Scaled_data)
+x=principal.transform(Scaled_data)
+print(x.shape)
+principal_df = pd.DataFrame(x, columns = ['PC1','PC2'])
+plt.figure(figsize = (6,6))
+sb.scatterplot(data = principal_df , x = 'PC1',y = 'PC2', s = 60 , palette= 'icefire')
+tend = datetime.datetime.now()
+plt.show()
+delta=tend-tstart
+print(delta)
