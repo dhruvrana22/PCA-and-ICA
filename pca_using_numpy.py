a06f8a9b740a52fd95011351d79355d255f91783
Dhruv Rana <dhruvrana2001@gmail.com>
2022-06-06 12:18:18 +0530

    Update pca_using_numpy.py

--- pca_using_numpy.py
+++ pca_using_numpy.py
@@ -13,15 +13,15 @@ def PCA(X , num_components):
     eigenvector_subset = sorted_eigenvectors[:,0:num_components]
     X_reduced = np.dot(eigenvector_subset.transpose() , X_meaned.transpose() ).transpose()
     return X_reduced
-
 tstart = datetime.datetime.now()
-data = pd.read_csv("eegData.csv")
+data = pd.read_csv("eegData.csv",header=None)
 x = data.iloc[:,0:16]
 mat_reduced = PCA(x , 2)
+tend = datetime.datetime.now()
+print(mat_reduced.shape)
 principal_df = pd.DataFrame(mat_reduced , columns = ['PC1','PC2'])
 plt.figure(figsize = (6,6))
 sb.scatterplot(data = principal_df , x = 'PC1',y = 'PC2', s = 60 , palette= 'icefire')
-tend = datetime.datetime.now()
 plt.show()
 delta=tend-tstart
 print(delta)
